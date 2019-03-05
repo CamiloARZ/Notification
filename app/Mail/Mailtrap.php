d7f13311a97838990ec1ccfba4e0660190d727c3
Denisse Urrutia <dcurrutia@ing.ucsc.cl>
2019-03-05 10:39:27 -0300

    funcion correo
    funcion correo base

---
+++ app/Mail/Mailtrap.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class Mailtrap extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('dcurrutia@ing.ucsc.cl', 'Envio de prueba')->view('mail')->to('dcurrutia@ing.ucsc.cl')->subject('hola');
+    }
+}
